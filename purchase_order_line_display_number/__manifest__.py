177d5b7ebfc6f9833f4b0937ffc7fd6633904d62
manu <manuel.regidor@sygel.es>
2024-04-25 09:04:18 +0200

    T-5142[ADD]purchase_ordeR_line_display_number: configurar numero lineas para mostrar en compras

---
+++ purchase_order_line_display_number/__manifest__.py
@@ -0,0 +1,25 @@
+# Copyright 2024 Manuel Regidor <manuel.regidor@sygel.es>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Purchase Order Line Display Number",
+    "summary": "Configure the number of purchase order lines to be shown",
+    "version": "15.0.1.0.0",
+    "category": "Purchase",
+    "website": "https://www.sygel.es",
+    "author": "Sygel, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        'purchase',
+    ],
+    "data": [
+        "data/purchase_order_line_display_number_data.xml"
+    ],
+    "assets": {
+        'web.assets_backend': [
+            'purchase_order_line_display_number/static/src/js/form_view.js',
+        ]
+    }
+}
