12e94484bb0ef755e4b87fc95b8a2b8e735a9bfc
sygel <alberto.martinez@sygel.es>
2024-11-15 11:21:36 +0100

    [T-6849][ADD] purchase_product_warn_message

---
+++ purchase_product_warn_message/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2024 Alberto Martínez <alberto.martinez@sygel.es>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Purchase Product Warn Message",
+    "summary": "Add a popup warning on purchase",
+    "version": "17.0.1.0.0",
+    "category": "Purchase",
+    "website": "https://github.com/sygel-technology/sy-purchase-workflow",
+    "author": "Sygel, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "purchase",
+    ],
+    "data": ["views/purchase_order_views.xml"],
+}
